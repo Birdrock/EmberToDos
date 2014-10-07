f88a25186022940637522d7e1c5992ba506563d8
Andrew Wittrock <awittrock@gmail.com>
2014-10-06 19:17:17 -0500

    added local storage for persistence

--- js/application.js
+++ js/application.js
@@ -1,3 +1,6 @@
 window.Todos = Ember.Application.create();
 // attached to window as a property rather than a variable
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+//Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
